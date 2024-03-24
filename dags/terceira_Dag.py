7aff17c676d145211f209330acb03ddccb465709
LucianoBorba <luhborbafilho@gmail.com>
2024-03-24 11:04:42 -0300

    feat: adicionando uma nova dag

---
+++ dags/terceira_Dag.py
@@ -0,0 +1,43 @@
+"""Terceira DAG."""
+from time import sleep
+
+from airflow.decorators import dag, task
+from datetime import datetime
+
+@dag(
+        dag_id='Minha Primeira Dag',
+        description='Primeira DAG',
+        schedule='* * * * *'
+        start_date=datetime(2024,3,24),
+        catchup=False
+)
+def pipeline():
+    @task
+    def primeira_atividade():
+        print('Minha primeira atividade')
+        sleep(2)
+
+    @task
+    def segunda_atividade():
+        print('Minha segunda atividade')
+        sleep(2)
+
+    @task
+    def terceira_atividade():
+        print('Minha terceira atividade')
+        sleep(2)
+
+    @task
+    def quarta_atividade():
+        print('Dag Finalizada')
+        sleep(2)
+
+
+    t1 = primeira_atividade()
+    t2 = segunda_atividade()
+    t3 = terceira_atividade()
+    t4 = quarta_atividade()
+
+    t1 >> t2 >> t3 >> t4
+
+pipeline()
